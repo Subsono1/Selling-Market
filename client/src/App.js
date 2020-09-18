f3e0cb319733dad7837f2782974cfef0d13eaea6
Sebastian Duarte <Subsono33@gmail.com>
2020-09-18 02:29:20 -0400

    created layout, header, footer and login screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,63 @@
-import React from 'react';
+import { Switch, Route, useHistory  } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 
 import './App.css';
+import Layout from './layout/Layout';
+import Login from './screens/Login'
+
+import {loginUser, registerUser, verifyUser, removeToken } from './services/auth'
+
+
+
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null)
+
+  useEffect(() => {
+    const handleVerify = async () => {
+      const userData = await verifyUser()
+      setCurrentUser(userData)
+    }
+    handleVerify()
+  }, [])
+
+  const loginSubmit = async (loginData) => {
+    const userData = await loginUser(loginData)
+    setCurrentUser(userData)
+    
+
+  }
+
+  const registerSubmit = async (registerData) => {
+    const userData = await registerUser(registerData)
+    setCurrentUser(userData)
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem('authToken')
+    removeToken()
+    setCurrentUser(null)
+  }
+  
   return (
-    <div className="App">
+    <div>
+    <Layout
+    currentUser={currentUser}
+      handleLogout={handleLogout}
+      >
+        </Layout> 
+
+    
+      <Switch>
+        <Route path='/'>
+          <Login loginSubmit={loginSubmit}/>
+          </Route>
+
+        </Switch>
+        
+      </div>
       
-    </div>
+
   );
 }
 
---
+++ client/src/screens/Login.jsx
@@ -0,0 +1,47 @@
+import { Input } from '@material-ui/core'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+
+function Login(props) {
+  const {loginSubmit} = props
+  const [formInfo, setFormInfo] = useState({
+    username: '',
+    password: ''
+  })
+
+  const { username, password } = formInfo
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormInfo(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+  
+  return (
+    <div className='login-div'>
+      <h2>Welcome! Please Login or Register</h2>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        loginSubmit(formInfo)
+      }}className='login-form'>
+    
+      
+        
+        <input type='text' name='username' value={username} placeholder='Please Enter User Name' onChange={handleChange}/>
+        
+        <input type='text' name='password' value={password} placeholder='Please Enter Your Password' onChange={handleChange}/>
+        
+        <button>Submit</button>
+        <br />
+       
+        <Link to='/register' className='register-link'>Register</Link>
+
+        </form>
+      </div>
+  )
+}
+
+export default Login
