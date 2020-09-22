977998401e02b9401bb434cc8f760c6e6cb66ae4
Sebastian Duarte <Subsono33@gmail.com>
2020-09-22 10:34:14 -0400

    did transfer to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import { Switch, Route, useHistory  } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
+import { getAllOrders } from './services/orders'
 
 import './App.css';
 import BuySell from './screens/BuySell';
 import Layout from './layout/Layout';
 import Register from './screens/Register'
 import Login from './screens/Login';
+import Checkout from './screens/Checkout'
 
 import { loginUser, registerUser, verifyUser, removeToken } from './services/auth';
+
 
 import Container from './container/Container';
 
@@ -19,6 +22,7 @@ import Container from './container/Container';
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
+  const [orders, setOrders] = useState([])
   const history = useHistory();
 // all auth functionalities should be listed here in the app since my layout is being called here. 
   useEffect(() => {
@@ -27,8 +31,17 @@ function App() {
       setCurrentUser(userData);
       // history.push("/");
     };
+
+    const fetchOrders = async () => {
+      const ordersArray = await getAllOrders()
+      setOrders(ordersArray)
+    }
     handleVerify();
+    fetchOrders()
+    
   }, []);
+
+  
 
   const loginSubmit = async (loginData) => {
     const userData = await loginUser(loginData);
@@ -73,12 +86,22 @@ function App() {
 
         </Route>
 
+        <Route exact path='/checkout'>
+        <Checkout
+          orders={orders}
+          
+          currentUser={currentUser}/>
+
+      </Route>
+
+
        
 
         <Route   path='/'>
           <Container
             currentUser={currentUser}/>
         </Route>
+
 
 
         </Switch>
